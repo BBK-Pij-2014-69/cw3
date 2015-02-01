451fc51631840f2760abc39b42ff6462146628aa
kieren millar <kieren_1@hotmail.com>
2015-02-01 22:40:28 +0000

    changed test to Junit test

--- SampleableListImplTest.java
+++ SampleableListImplTest.java
@@ -1,5 +1,11 @@
 package cw3;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * 
  * @author Kieren Millar
@@ -7,20 +13,63 @@ package cw3;
  */
 public class SampleableListImplTest {
 
-	public static void main(String[] args) {
-		LinkedList listA = new LinkedList();
-		ArrayList listB = new ArrayList();
-		SampleableListImpl SampleableLinkedList = new SampleableListImpl(listA);
-		SampleableListImpl SampleableArrayList = new SampleableListImpl(listB);
-		System.out.println((SampleableLinkedList.sample().isEmpty() == true)? "true" : "false");
+	LinkedList listA = new LinkedList();
+	ArrayList listB = new ArrayList();
+	SampleableListImpl SampleableLinkedList = new SampleableListImpl(listA);
+	SampleableListImpl SampleableArrayList = new SampleableListImpl(listB);
+	
+	@Before
+	public void setup(){
 		SampleableArrayList.add("First");
 		SampleableArrayList.add("Seconed");
 		SampleableArrayList.add("Third");
 		SampleableArrayList.add("Fourth");
 		SampleableArrayList.add("Fifth");
-		SampleableList result = new SampleableListImpl(SampleableArrayList.sample());
-		for (int i = 0; i < result.size(); i++){
-			System.out.println(result.get(i).getReturnValue());
-		}
 	}
+	
+	@After
+	public void takeDown(){
+		SampleableLinkedList = new SampleableListImpl(listA);
+		SampleableArrayList = new SampleableListImpl(listB);
+	}
+	
+	@Test
+	public void testIsEmpty() {
+		assertTrue(SampleableLinkedList.sample().isEmpty());
+	}
+
+	@Test
+	public void testSize() {
+		assertEquals(SampleableArrayList.size(), 5);
+	}
+
+	@Test
+	public void testGet() {
+		assertEquals(SampleableArrayList.get(2).getReturnValue(), "Third");
+	}
+
+	@Test
+	public void testRemove() {
+		assertEquals(SampleableArrayList.remove(2).getReturnValue(), "Third");
+	}
+
+	@Test
+	public void testAddIntObject() {
+		assertEquals(SampleableArrayList.add(2, "third").getError(), ErrorMessage.NO_ERROR);
+	}
+
+	@Test
+	public void testAddObject() {
+		assertEquals(SampleableArrayList.add("Sixth").getError(), ErrorMessage.NO_ERROR);
+	}
+
+	@Test
+	public void testSample() {
+		SampleableList one = SampleableArrayList.sample();
+		assertEquals(one.size(), 3);
+		assertEquals(one.get(0).getReturnValue(), "First");
+		assertEquals(one.get(1).getReturnValue(), "Third");
+		assertEquals(one.get(2).getReturnValue(), "Fifth");
+	}
+
 }
